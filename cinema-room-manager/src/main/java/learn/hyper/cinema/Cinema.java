47e9ef361c40a579b8944b361f975721ef53ebf9
Denis Popkov <pkvdenis@gmail.com>
2021-12-16 00:09:16 +0300

    cinema-room-manager: stage 2 of 5 - calculate income.

--- cinema-room-manager/src/main/java/learn/hyper/cinema/Cinema.java
+++ cinema-room-manager/src/main/java/learn/hyper/cinema/Cinema.java
@@ -1,8 +1,32 @@
 package learn.hyper.cinema;
 
+import java.util.Scanner;
+
 public class Cinema {
+
     public static void main(String[] args) {
-        System.out.print(makeRoom(7, 8));
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number of rows:");
+        int rows = in.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int seats = in.nextInt();
+        System.out.println("Total income:");
+        int income = calculateIncome(rows, seats);
+        System.out.println("$" + income);
+    }
+
+    public static int calculateIncome(int rows, int seats) {
+        final int frontSeatsPrice = 10;
+        final int backSeatsPrice = 8;
+        int income;
+        if (rows * seats <= 60) {
+            income = rows * seats * frontSeatsPrice;
+        } else {
+            int numFrontRows = rows / 2;
+            int numBackRows = rows - numFrontRows;
+            income = seats * (numFrontRows * frontSeatsPrice + numBackRows * backSeatsPrice);
+        }
+        return income;
     }
 
     public static String makeRoom(int rows, int seats) {
--- cinema-room-manager/src/test/java/learn/hyper/cinema/CinemaTest.java
+++ cinema-room-manager/src/test/java/learn/hyper/cinema/CinemaTest.java
@@ -1,33 +1,22 @@
 package learn.hyper.cinema;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
-
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CinemaTest {
 
-    @Test
-    void testMain() {
-        String expected = ""
-                + "Cinema:\n"
-                + "  1 2 3 4 5 6 7 8\n"
-                + "1 S S S S S S S S\n"
-                + "2 S S S S S S S S\n"
-                + "3 S S S S S S S S\n"
-                + "4 S S S S S S S S\n"
-                + "5 S S S S S S S S\n"
-                + "6 S S S S S S S S\n"
-                + "7 S S S S S S S S\n";
-        final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-        final PrintStream savedOut = System.out;
-        System.setOut(new PrintStream(buffer));
-        Cinema.main(null);
-        System.setOut(savedOut);
-        String actual = buffer.toString();
-        assertEquals(expected, actual);
+    @ParameterizedTest
+    @CsvSource({
+            "4, 5, 200",
+            "8, 9, 648",
+            "9, 7, 560"
+    })
+    void testCalculateIncome(int rows, int seats, int expectedIncome) {
+        int income = Cinema.calculateIncome(rows, seats);
+        assertEquals(expectedIncome, income);
     }
 
     @Test
