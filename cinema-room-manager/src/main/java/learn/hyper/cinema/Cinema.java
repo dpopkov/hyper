f68c490e92d89f1d6f81dc2d32088a99bb30c0cb
Denis Popkov <pkvdenis@gmail.com>
2021-12-25 02:44:36 +0300

    cinema-room-manager: refactor Cinema to object.

--- cinema-room-manager/src/main/java/learn/hyper/cinema/Cinema.java
+++ cinema-room-manager/src/main/java/learn/hyper/cinema/Cinema.java
@@ -8,33 +8,50 @@ public class Cinema {
     public static final int BACK_SEATS_PRICE = 8;
     public static final int NUM_SEATS_FOR_SMALL_ROOM = 60;
 
+    private final Room room;
+    private final Calculator calculator;
+
+    public Cinema(int rows, int seats) {
+        room = new Room(rows, seats);
+        calculator = new Calculator(FRONT_SEATS_PRICE, BACK_SEATS_PRICE, NUM_SEATS_FOR_SMALL_ROOM);
+    }
+
+    public void printArrangement() {
+        System.out.println(room.formatAsString("Cinema:"));
+    }
+
+    public int priceForSeat(int rowNumber) {
+        return calculator.ticketPrice(rowNumber, room);
+    }
+
+    public void reserveSeat(int rowNumber, int seatNumber) {
+        room.reserveSeat(rowNumber, seatNumber);
+    }
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter the number of rows:");
-        int rows = in.nextInt();
-        System.out.println("Enter the number of seats in each row:");
-        int seats = in.nextInt();
+        final Scanner in = new Scanner(System.in);
+        int rows = readInt("Enter the number of rows:", in);
+        int seats = readInt("Enter the number of seats in each row:", in);
         System.out.println();
 
-        // Print seating arrangement
-        Room room = new Room(rows, seats);
-        System.out.println(room.formatAsString("Cinema:"));
+        Cinema cinema = new Cinema(rows, seats);
+        cinema.printArrangement();
 
         // Read coordinates of the seat to print the ticket price
-        System.out.println("Enter a row number:");
-        int rowNumber = in.nextInt();
-        System.out.println("Enter a seat number in that row:");
-        int seatNumber = in.nextInt();
+        int rowNumber = readInt("Enter a row number:", in);
+        int seatNumber = readInt("Enter a seat number in that row:", in);
         System.out.println();
 
-        // Print the ticket price
-        Calculator calculator = new Calculator(FRONT_SEATS_PRICE, BACK_SEATS_PRICE, NUM_SEATS_FOR_SMALL_ROOM);
-        int price = calculator.ticketPrice(rowNumber, room);
+        int price = cinema.priceForSeat(rowNumber);
         System.out.printf("Ticket price: $%d%n", price);
         System.out.println();
 
-        // Print cinema with chosen seat marked 'B'
-        room.reserveSeat(rowNumber, seatNumber);
-        System.out.println(room.formatAsString("Cinema:"));
+        cinema.reserveSeat(rowNumber, seatNumber);
+        cinema.printArrangement();
+    }
+
+    private static int readInt(String prompt, Scanner in) {
+        System.out.println(prompt);
+        return in.nextInt();
     }
 }
