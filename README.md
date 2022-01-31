987e2e491abf1f13025017b077c6cfa3f640b7b6
Denis Popkov <pkvdenis@gmail.com>
2022-01-31 12:30:21 +0300

    simple-chatty-bot: stage 1 - Hello.

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 
 ### Easy
 
+#### [Simple Chatty Bot](simple-chatty-bot)
 #### [Cinema Room Manager](cinema-room-manager)
 #### [Simple Tic-Tac-Toe](simple-tic-tac-toe)
---
+++ simple-chatty-bot/src/main/java/learn/hyper/chattybot/SimpleBot.java
@@ -0,0 +1,8 @@
+package learn.hyper.chattybot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2020.");
+    }
+}
